a9b767ff05eb36be4b93ecbc26ca7875013c0e57
shunkode <106649051+shunkode@users.noreply.github.com>
2024-02-06 14:57:08 +0900

    Delete MotionDetection_with_yolov5/test.py

--- MotionDetection_with_yolov5/test.py
+++
@@ -1,8 +0,0 @@
-from detect import run as yolov5_detect  # Assuming you have a callable YOLOv5 function
-
-video_path = 'test.h264'
-
-
-yolov5_detect(weights="../weights/day_weight/best.pt", 
-              source="test.h264")
-
